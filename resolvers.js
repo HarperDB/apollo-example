0cf87dacce640a54cfcf94c4485528c773aa10a7
Kyle Bernhardy <kyle@harperdb.io>
2024-02-23 10:58:03 -0700

    Added logic to convert GraphQL selected fields and conditions to HarperDB gets and conditions.
    Added inputs to schema to demonstrate how they can be used in HarperDB.
    
    Added example of how to delegate auth to HarperDB

--- resolvers.js
+++ resolvers.js
@@ -4,37 +4,42 @@ const {Dog, Breed} = databases.demo;
 const resolvers = {
 	Query: {
 		dogs: async (parent, args, context, info) =>{
-			const dogs = await Dog.search({
-				conditions: [
-					{ attribute: 'id', value: -1, comparator: 'greater_than' }
-				]
-			});
-			return dogs;
+			let conditions = generateConditions(args?.dog);
+			let search = {select:createSelect(info?.fieldNodes["0"]?.selectionSet?.selections)};
+			if(conditions.length > 0) {
+				search.operator = conditions.length > 1 ? 'or' : 'and';
+				search.conditions = conditions;
+			}
+
+			return Dog.search(search);
 		},
 		dog: async (parent, args, context, info) => {
-			let id = Number(args.id)
-			return await Dog.get(id);
+			return Dog.get({id: args.id, select:createSelect(info?.fieldNodes["0"]?.selectionSet?.selections)});
 		},
 		dogsByBreedName: async (parent, args, context, info) => {
-			let results = await Dog.search({
+			//here we show how to delegate authorization to harperdb
+			//this line instructs HarperDB to authorize the search on the context
+			context.authorize = true;
+			//passing the context gives further instructions to HarperDB, in this case telling it to authorize the search
+			return  Dog.search({
 				conditions: [
 					{ attribute: 'breedName', value: args.breedName, comparator: 'equal' }
-				]
-			});
+				],
 
-			return results;
+			}, context);
 		},
 		breeds: async (parent, args, context, info) =>{
-			let breeds = Breed.search({
-				conditions: [
-					{ attribute: 'name', value: -1, comparator: 'greater_than' }
-				]
-			});
-			return breeds;
+			let conditions = generateConditions(args?.dog);
+			let search = {select:createSelect(info?.fieldNodes["0"]?.selectionSet?.selections)};
+			if(conditions.length > 0) {
+				search.operator = conditions.length > 1 ? 'or' : 'and';
+				search.conditions = conditions;
+			}
+			return Breed.search(search);
 		},
 		breed: async (parent, args, context, info) => {
 			args.name = args.name.toLowerCase();
-			return Breed.get(args.name);
+			return Breed.get({name: args.name, select:createSelect(info?.fieldNodes["0"]?.selectionSet?.selections)});
 		},
 	},
 	Mutation: {
@@ -46,40 +51,60 @@ const resolvers = {
 				throw Error(`invalid breedName '${args.breedName}', breed not found. `)
 			}
 
-			args.id = Number(args.id);
-			args.dob = args.dob ? new Date(args.dob): undefined
 			//setting the name to exactly match the casing from the breed entry
 			args.breedName = breed.name;
 			await Dog.put(args);
 			return args;
 		},
 		deleteDog: async (parent, args, context, info) =>{
-			const id = Number(args.id);
-			await Dog.delete(id);
+			return Dog.delete(args.id);
 		}
 	},
-	Dog: {
-		breed(parent) {
-			return Breed.get(parent.breedName);
-		}
-	},
-	Breed: {
-		async dogs(parent) {
-			let results = await Dog.search({
-				conditions: [
-					{ attribute: 'breedName', value: parent.name, comparator: 'equal' }
-				]
-			});
-
-			let dogs = [];
-			for await (let dog of results) {
-				dogs.push(dog)
-			}
-
-			return dogs;
-		}
-	}
 
 };
+
+/**
+ * This is a generic example of how to generate a condition array from an args object
+ * @param args
+ * @param prefix
+ * @param conditions
+ * @returns {*[]}
+ */
+function generateConditions(args, prefix = [], conditions = []) {
+	if(!args) {
+		return conditions;
+	}
+
+	for (const [key, value] of Object.entries(args)) {
+		if(typeof value === "object") {
+			generateConditions(value, prefix.concat([key]), conditions);
+		} else {
+			conditions.push({attribute: prefix.concat([key]), value, comparator: 'equal' })
+		}
+	}
+	return conditions;
+}
+
+function createSelect(selections, relationshipName, select = []) {
+	let relation;
+	if(relationshipName) {
+		relation = {name: relationshipName, select: []};
+		select.push(relation);
+	}
+
+	selections.forEach(item=>{
+		if(item.selectionSet) {
+			createSelect(item.selectionSet.selections, item.name.value, select);
+		} else {
+			if(relationshipName) {
+				relation.select.push(item.name.value);
+			} else {
+				select.push(item.name.value);
+			}
+		}
+	});
+
+	return select;
+}
 
 export default resolvers;
